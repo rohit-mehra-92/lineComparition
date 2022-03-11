e9f5fff9ed4a640af79e2d660c7a2db85733aaca
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-12 03:53:37 +0530

    Added:checking equality of two lines

--- src/main/line.java
+++ src/main/line.java
@@ -3,23 +3,46 @@ import java.util.Scanner;
 public class line {
     public static void main(String[] args) {
 
-        int x1,y1,x2,y2;
-        double distance;
-        Scanner sc= new Scanner(System.in);
+        int x1, y1, x2, y2;
+        double line1;
+
+        Scanner num = new Scanner(System.in);
         System.out.println("enter the value of x1");
-            x1 = sc.nextInt();
+        x1 = num.nextInt();
 
         System.out.println("enter the value of y1");
-            y1 = sc.nextInt();
+        y1 = num.nextInt();
 
         System.out.println("enter the value of x2");
-            x2 = sc.nextInt();
+        x2 = num.nextInt();
 
         System.out.println("enter the value of y2");
-            y2 = sc.nextInt();
+        y2 = num.nextInt();
 
-        distance = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-        System.out.println("distance between two line is: " + distance);
+        line1 = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("Length of the line1 is :" + line1);
+
+        int a1,b1,a2,b2;
+        double line2;
+        System.out.println("enter the value of a1");
+        a1 = num.nextInt();
+
+        System.out.println("enter the value of b1");
+        b1 = num.nextInt();
+
+        System.out.println("enter the value of a2");
+        a2 = num.nextInt();
+
+        System.out.println("enter the value of b2");
+        b2 = num.nextInt();
+
+        line2 = Math.sqrt((a2-a1)^2 + (b2-b1)^2);
+        System.out.println("Length of the line2 is :" + line2);
+
+        if (line1 == line2)
+            System.out.println("line are equal");
+        else
+            System.out.println("line are not equal");
+
     }
 }
-
