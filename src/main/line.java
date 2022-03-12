ee0ac7385f9ced41a0378c90a0d1fc2523a37e9f
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-12 17:27:51 +0530

    Added:Comparison of lines using equals() and compareTo() method

--- src/main/line.java
+++ src/main/line.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class line {
     public static void main(String[] args) {
 
-        Double x1, y1, x2, y2;
+        double x1, y1, x2, y2;
         Double line1;
 
         Scanner num = new Scanner(System.in);
@@ -23,7 +23,7 @@ public class line {
         line1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
         System.out.println("Length of the line1 is :" + line1);
 
-        Double a1,b1,a2,b2;
+        double a1,b1,a2,b2;
         Double line2;
         System.out.println("enter the value of a1");
         a1 = num.nextDouble();
@@ -40,11 +40,19 @@ public class line {
         line2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
         System.out.println("Length of the line2 is :" + line2);
 
-        if (line1.compareTo(line2)==0)
-            System.out.println("lines are equal");
-        else if (line1.compareTo(line2)>0)
-            System.out.println("lines1 is greater than line2");
-        else if (line1.compareTo(line2)<0)
-            System.out.println("line1 is smaller than line2");
+
+          if(line1.equals(line2))
+              System.out.println("lines are equal");
+          else
+              System.out.println("lines are not equal");
+
+
+          if (line1.compareTo(line2)==0)
+                System.out.println("lines are equal");
+            else if (line1.compareTo(line2)>0)
+                System.out.println("lines1 is greater than line2");
+            else if (line1.compareTo(line2)<0)
+                System.out.println("line1 is smaller than line2");
+
     }
 }
