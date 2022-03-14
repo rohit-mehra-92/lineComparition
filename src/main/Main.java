9cb8364cbd321a0a93b7a9a531cb04e3fab32078
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-14 09:59:19 +0530

    added:get lenght method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -22,9 +21,10 @@ public class Main {
         System.out.println("enter the value of y2");
         y2 = num.nextDouble();
 
-        line1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        Main main = new Main();
+        line1 = main.getLenght(x1, x2, y1, y2);
 
-        double a1,b1,a2,b2;
+        double a1, b1, a2, b2;
         Double line2;
         System.out.println("enter the value of a1");
         a1 = num.nextDouble();
@@ -38,24 +38,23 @@ public class Main {
         System.out.println("enter the value of b2");
         b2 = num.nextDouble();
 
-        line2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+        line2 = main.getLenght(a1, a2, b1, b2);
 
         System.out.println("Length of the line1 is :" + line1);
         System.out.println("Length of the line2 is :" + line2);
 
-
-          if(line1.equals(line2))
-              System.out.println("lines are equal");
-          else
-              System.out.println("lines are not equal");
-
-
-          if (line1.compareTo(line2)==0)
-                System.out.println("lines are equal");
-            else if (line1.compareTo(line2)>0)
-                System.out.println("lines1 is greater than line2");
-            else if (line1.compareTo(line2)<0)
-                System.out.println("line1 is smaller than line2");
+        if (line1.compareTo(line2) == 0)
+            System.out.println("lines are equal");
+        else if (line1.compareTo(line2) > 0)
+            System.out.println("lines1 is greater than line2");
+        else if (line1.compareTo(line2) < 0)
+            System.out.println("line1 is smaller than line2");
 
     }
+
+    Double getLenght(double x1, double x2, double y2, double y1) {
+        Double value = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return value;
+    }
 }
+
